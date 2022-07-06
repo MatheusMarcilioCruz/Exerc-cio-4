197d878970da7a389a844e13ff78099c087b7aad
Matah <Matah@DESKTOP-5VBIVJF>
2022-07-06 15:52:36 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios Djalma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            {
+
+                for (int i = 1; i <= 50; i++)
+
+                {
+
+                    Console.WriteLine($"Number: {i}");
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
